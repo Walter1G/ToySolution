66aba5a0ee3b1f31602721d60b428f220d0d0805
Walter N <2760walter@gmail.com>
2023-09-24 10:46:43 +0300

    finish student grade generator

---
+++ index.js
@@ -0,0 +1,33 @@
+const gradebtn = document.getElementById("gradebtn");
+
+// Use an event listener to call assignGrade when the button is clicked
+gradebtn.addEventListener("click", assignGrade);
+
+function assignGrade(e) {
+    // Get the marks as a number from the input field, not from innerHTML
+    const marks = parseFloat(document.getElementById("markstxt").value);
+    
+
+    // Validate the input
+    if (isNaN(marks) || marks < 0 || marks > 100) {
+        document.getElementById("gradetxt").value = "Enter marks between 0 - 100 %";
+        return;
+    }
+
+    let grade;
+    //Determine the grade
+    if (marks > 79) {
+        grade = "A";
+    } else if (marks >= 60 && marks <= 79) {
+        grade = "B";
+    } else if (marks >= 50 && marks <= 59) {
+        grade = "C";
+    } else if (marks >= 40 && marks <= 49) {
+        grade = "D";
+    } else {
+        grade = "E";
+    }
+
+    // Display the grade
+    document.getElementById("gradetxt").value = `${grade}`;
+}
